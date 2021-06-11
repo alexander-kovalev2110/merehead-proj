eda29639ef68c121919400d3d2a3c0f6583c3a65
alexander-kovalev2110 <alexander.kovalev@yahoo.com>
2021-06-11 22:40:01 +0300

    Updates

---
+++ precache-manifest.a362e0a959c4e1bab0da11f6efcee49a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "25717f34b7e717b800d49bf2b70e49d5",
+    "url": "/alexander-kovalev2110/merehead-proj/index.html"
+  },
+  {
+    "revision": "372aedf1b7350a8c2f24",
+    "url": "/alexander-kovalev2110/merehead-proj/static/css/main.f4d6f893.chunk.css"
+  },
+  {
+    "revision": "4e990b164f1ed306c722",
+    "url": "/alexander-kovalev2110/merehead-proj/static/js/2.904cfc97.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/alexander-kovalev2110/merehead-proj/static/js/2.904cfc97.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "372aedf1b7350a8c2f24",
+    "url": "/alexander-kovalev2110/merehead-proj/static/js/main.07ee3eeb.chunk.js"
+  },
+  {
+    "revision": "16e31f8831c20f3c934a",
+    "url": "/alexander-kovalev2110/merehead-proj/static/js/runtime-main.3b08145a.js"
+  }
+]);
